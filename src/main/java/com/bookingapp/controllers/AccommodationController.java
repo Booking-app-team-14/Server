36565f70f2e727a5ddc287e4b1d7e620907b43bb
Vuk Dimitrov <vuk.dimitrov@gmail.com>
2023-11-30 01:26:20 +0100

    Fixed mocking for POSTMAN requests.

--- src/main/java/com/bookingapp/controllers/AccommodationController.java
+++ src/main/java/com/bookingapp/controllers/AccommodationController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/accommodations")
+@RequestMapping("api/accommodations")
 public class AccommodationController {
 
     private final AccommodationService accommodationService;
